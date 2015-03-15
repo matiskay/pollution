6a375caabb607b0a1f86795a1c7f85e4eb6556db
Edgar Marca <matiskay@gmail.com>
2015-03-14 21:59:23 -0300

    Add matrix_distance function

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include "matrix.h"
 
 /* Add function signatures here */
+float matrix_distance(Matrix* mat1, Matrix* mat2);
 
 int main(int argc, char **argv) {
   Matrix* mat;
@@ -26,4 +28,22 @@ int main(int argc, char **argv) {
   matrix_free(mat);
 
   return 0;
+}
+
+
+float matrix_distance(Matrix* mat1, Matrix* mat2) {
+  /* Check the dimension of the matrices */
+  float max = 0;
+  float current_diff;
+
+  for (i = 0; i < mat1->number_of_rows; i++) {
+    for (j = 0; j < mat1->number_of_columns; j++) {
+      current_diff = abs(matrix_get(mat1, i, j) - matrix_get(mat2, i, j));
+      if (current_diff > max) {
+        max = current_diff;
+      }
+    }
+  }
+
+  return max;
 }
